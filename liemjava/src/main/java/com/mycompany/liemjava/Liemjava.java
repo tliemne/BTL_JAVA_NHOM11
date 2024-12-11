299044ee5989adf528df11ef16bd83720783639b
tliemne <140905478+tliemne@users.noreply.github.com>
2024-12-11 23:38:27 +0700

    gg

---
+++ liemjava/src/main/java/com/mycompany/liemjava/Liemjava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.liemjava;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Liemjava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
